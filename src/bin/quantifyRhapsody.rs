202a69972010db0b698c3d5f7cc1dcf416107547
Stefan Lang <stefan.lang@med.lu.se>
2023-01-26 12:57:36 +0100

    Version changed to 0.3.3

--- src/bin/quantifyRhapsody.rs
+++ src/bin/quantifyRhapsody.rs
@@ -29,7 +29,7 @@ use std::io::Write;
 /// You need quite long R1 and R2 reads for this! (>70R1 and >70R2 [v1] and 52 bp reads for v2.96 and v2.384)
 
 #[derive(Parser)]
-#[clap(version = "0.3.1", author = "Stefan L. <stefan.lang@med.lu.se>")]
+#[clap(version = "0.3.3", author = "Stefan L. <stefan.lang@med.lu.se>")]
 struct Opts {
     /// the input R1 reads file
     #[clap(short, long)]
