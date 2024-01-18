2e620ccb34b46f6dde36ef2faa9c48d2ef69c454
Stefan Lang <stefan.lang@med.lu.se>
2024-01-18 09:22:08 +0100

    removes the debug message.

--- src/fast_mapper/fast_mapper.rs
+++ src/fast_mapper/fast_mapper.rs
@@ -755,16 +755,17 @@ impl FastMapper{
         if genes.len() == 0 {
             return None
         }
-        let bad_gene = "Lin28b";
-        let bad = self.get_id( bad_gene.to_string()) ;
-        //eprintln!("Here I have these gene counts: {:?}", genes );
+        
+        /*let bad_gene = "Lin28b";
+        let bad = self.get_id( bad_gene.to_string()) ;*/
+
         // check if there is only one gene //
         if self.get_best_gene( &genes, &mut matching_geneids ){
             //println!("I have these genes: {genes:?} And am returning: {:?}",  matching_geneids);
-            if matching_geneids[0] == bad {
+            /*if matching_geneids[0] == bad {
                 eprintln!("read mapping to {} - should not happen here!: {:?}\n{:?}", bad_gene, self.gene_names_for_ids( &matching_geneids ),String::from_utf8_lossy(seq) );
                 eprintln!("This is our total matching set: {:?}", genes);
-            }
+            }*/
             return Some( matching_geneids )
         }
         if matching_geneids.len() > 2{
