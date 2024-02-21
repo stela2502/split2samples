bb82dae6f40aa3d0faab4f8c27acc2bf5b348bae
Stefan Lang <Stefan.Lang@med.lu.se>
2024-02-21 09:06:02 +0100

    SampleID look GOOD!
    But the gene mapping lacks?

---
+++ copy_to_sens5.sh
@@ -0,0 +1,10 @@
+cp target/release/split2samples ~/sens05_home/bin/
+cp target/release/quantify_rhapsody ~/sens05_home/bin/
+cp target/release/quantify_rhapsody_multi ~/sens05_home/bin/
+cp target/release/bd_cell_id_2_seq ~/sens05_home/bin/
+cp target/release/bd_get_single_cell ~/sens05_home/bin/
+cp target/release/get_n_cell_reads ~/sens05_home/bin/
+cp target/release/int_2_seq ~/sens05_home/bin/
+cp target/release/create_index ~/sens05_home/bin/
+cp target/release/create_index_te ~/sens05_home/bin/
+cp target/release/te_analysis ~/sens05_home/bin/
