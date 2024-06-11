08281e857c61142b2cac7e9133df5d3dc2485564
Stefan Lang <Stefan.Lang@med.lu.se>
2024-06-11 12:44:08 +0200

    one more test case.

--- copy_to_sens5.sh
+++ copy_to_sens5.sh
@@ -1,8 +1,8 @@
 cp target/release/bd_cell_id_2_seq ~/sens05_home/bin
 cp target/release/bd_get_single_cell ~/sens05_home/bin
-cp target/release/check_read_fast_mapper ~/sens05_home/bin
-cp target/release/check_read_gene_mapper ~/sens05_home/bin
-cp target/release/check_read_genomic_mapper ~/sens05_home/bin
+cp target/debug/check_read_fast_mapper ~/sens05_home/bin
+cp target/debug/check_read_gene_mapper ~/sens05_home/bin
+cp target/debug/check_read_genomic_mapper ~/sens05_home/bin
 cp target/release/create_gene_mapper_index ~/sens05_home/bin
 cp target/release/create_index ~/sens05_home/bin
 cp target/release/create_index_te ~/sens05_home/bin
